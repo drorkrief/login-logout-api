571079f6ddafe88e530d2a6f70844fdef0a14b3c
dror <dror@ardon.co.il>
2023-01-03 12:41:14 +0200

    cors add more origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    'https://localhost:3000',
+    'https://www.login-logout-front.onrender.com',
     'https://login-logout-front.onrender.com'
 ]
 
