9211a791b5e0e573235fdf6c61c657a42d2315fc
Dror Kriaf <drork@ardon.local>
2024-01-13 22:51:08 +0200

    support ardon mail

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,11 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://localhost:3000',
-    'https://www.login-logout-front.onrender.com',
-    'https://login-logout-front.onrender.com'
-]
+  "http://localhost:3000",
+  "https://localhost:3000",
+  "http://localhost:5173",
+  "https://localhost:5173",
+  "https://psgot-team.onrender.com",
+  "https://www.login-logout-front.onrender.com",
+  "https://login-logout-front.onrender.com",
+];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
